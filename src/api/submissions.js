717400cb99c93ebbd207e59127469d11b855244a
buntinj <buntinj@oregonstate.edu>
2019-06-12 10:35:53 -0700

    Update submissions.js

--- src/api/submissions.js
+++ src/api/submissions.js
@@ -10,8 +10,8 @@ router.get('/:id.pdf', validateJwt, getRole, async (req, res, next) => {
   try{
     if(req.role === "admin" || req.role === "instructor" || req.role === "student" ){
       const submission = await getSubmissionbyID(parseInt(req.params.id));
-      if(submission){
-        res.status(200).type('application/pdf').send(submission.fileData);
+      if(submission && submission.fileData && submission.mimeType){
+        res.status(200).type(submission.mimeType).send(submission.fileData);
        }
      }
   } catch (err) {
